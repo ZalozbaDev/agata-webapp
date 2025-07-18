2cf7275a6c453c6dca88a1c18e4bd4c2756f3314
Kilian <tablettonisaekbautzen@gmail.com>
2025-07-18 16:42:53 +0200

    update starttilearray with new titles for improved user guidance

--- src/pages/start/index.tsx
+++ src/pages/start/index.tsx
@@ -11,8 +11,8 @@ const starttilearray: { title: string }[] = [
   { title: 'Budź chwaleny Jězus Chrystus.' },
   { title: 'Pomhaj Bóh.' },
   { title: 'Witaj, rjenje zo mje wopytaš ☺️' },
-  { title: 'Kak móžu Ći pomhać?' },
-  { title: 'Zapodaj prošu Twoje prašenje:' },
+  { title: 'Klik mi na mjezwočo, potom so powjetšu' },
+  { title: 'Zapodaj Twoje prašenje prošu w porjadnej serbšćinje' },
 ]
 
 const fadeDuration = 500
