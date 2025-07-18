a93552af470ff897c39530480071b185280d64a4
Kilian <tablettonisaekbautzen@gmail.com>
2025-07-18 10:12:22 +0200

    some impresum stuff

--- src/pages/impresum/index.tsx
+++ src/pages/impresum/index.tsx
@@ -11,9 +11,9 @@ const ImpresumPage: React.FC = () => {
     <div>
       <h1 style={impresumtitlestyle}>Impresum</h1>
       <h2 style={podnadpisma}>Sobuskutkowacy:</h2>
-      <p style={pismo}>Beno Baier, Feliks Šołta, MiC, Jan</p>
+      <p style={pismo}>Beno Baier, Feliks Šołta, MiC, Jan Breindl</p>
       <h2 style={podnadpisma}>Programy, kotrež wužiwamy:</h2>
-      <p style={pismo}>ChatGPT, Bamborak, Ocr wot Wita Bejmaka</p>
+      <p style={pismo}>ChatGPT, Bamborak, Ocr wot Wita Bejmaka, Inkubator</p>
     </div>
   )
 }
