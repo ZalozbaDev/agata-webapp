a2eb57d655f52fd7e7f431da6612f8728b9340e4
Kilian <tablettonisaekbautzen@gmail.com>
2025-07-19 08:51:35 +0200

    fix: improve phrasing and clarity in FAQ section

--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -19,10 +19,10 @@ const FaqPage: React.FC = () => {
       <p style={pismo}>Pruwuj wótrosć wótřerěčaka na swojim nastroju.</p>
       <br />
       <h2 style={podnadpisma}>Agata nima prawu wotmołwu na prašenje.</h2>
-      <p style={pismo}>Agata hišće wšitko, wona pak wuknje.</p>
+      <p style={pismo}>Agata hišće wšitko njewě, wona pak wuknje.</p>
       <br />
       <h2 style={podnadpisma}>Kak hasnu Agatu abo wotkazy?</h2>
-      <p style={pismo}>W nastajenjach.</p>
+      <p style={pismo}>W nastajenjach - horjeka na prawo.</p>
       <br />
       <h2 style={podnadpisma}>Mikrofon njefunguje.</h2>
       <p style={pismo}>Dyrbiš Agaće wužiwanje mikrofona dowolić.</p>
@@ -33,7 +33,7 @@ const FaqPage: React.FC = () => {
       <h2 style={podnadpisma}>Njemóžu wotmołwy čitać, su pře małke.</h2>
       <p style={pismo}>Změn ZOOM faktor wobrazowki, na kompjuteru z STRG +</p>
       <br />
-      <h2 style={podnadpisma}>Maš dalše prašenja?<br /> Kontaktěruj nas přez mailku <a style={{textDecoration: 'none'}} href={`mailto:agata@lucija.de`}>agata@lucija.de</a></h2>
+      <h2 style={podnadpisma}>Maš dalše prašenja?<br /> Kontaktěruj nas přez mailku <a style={{color: 'green', textDecoration: 'none'}} href={`mailto:agata@lucija.de`}>agata@lucija.de</a></h2>
       <h2 style={{...podnadpisma, color: 'red', fontWeight: '1000'}}>Wutrobny dźak za pokiwy a konstruktiwne prašenja.</h2>
     </div>
   )
