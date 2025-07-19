3a1c64acd974133710a621f5b36d633abf82a493
Kilian <tablettonisaekbautzen@gmail.com>
2025-07-19 10:07:40 +0200

    fix: FAQ pismo centrěrowane

--- src/pages/faq/styles.ts
+++ src/pages/faq/styles.ts
@@ -3,16 +3,19 @@ fontSize: '2.5rem',
 fontFamily: 'Arial, sans-serif',
 textDecoration: 'underline',
 color: 'white',
+textAlign: 'center',
 }
 
 export const podnadpisma: React.CSSProperties = {
 fontSize: '2.rem',
 fontFamily: 'Arial, sans-serif',
 color: '#f1fcffff',
+textAlign: 'center',
 }
 
 export const pismo: React.CSSProperties = {
 fontSize: '1.5rem',
 fontFamily: 'Arial, sans-serif',
 color: '#ffffffff',
+textAlign: 'center',
 }
