e97286d2d3ede10e979a7014d0434d97654341df
Kilian <tablettonisaekbautzen@gmail.com>
2025-07-19 08:27:25 +0200

    fix: correct spelling of 'Michał Cyž' in impresum page

--- src/pages/impresum/index.tsx
+++ src/pages/impresum/index.tsx
@@ -15,7 +15,7 @@ const ImpresumPage: React.FC = () => {
       <h1 style={impresumtitlestyle}>impresum</h1>
           <br />
       <h2 style={podnadpisma}>sobuskutkowacy:</h2>
-      <p style={pismo}>Beno Baier, Feliks Šołta,     <br />Michał Bjarnat Cyž, Jan Breindl</p>
+      <p style={pismo}>Beno Baier, Feliks Šołta,     <br />Michał Cyž, Jan Breindl</p>
           <br />
       <h2 style={podnadpisma}>programy, kotrež wužiwamy:</h2>
       <p style={pismo}>ChatGPT, TTS Bamborak, Sotra   <br />OCR Wita Bejmaka, STT Inkubator</p>
