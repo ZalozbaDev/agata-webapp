5db9a6f71c6b20ac8507c21e0485eac13c875ce2
Kilian <tablettonisaekbautzen@gmail.com>
2025-07-21 20:03:01 +0200

    test cyan za LINKs

--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -33,7 +33,7 @@ const FaqPage: React.FC = () => {
       <h2 style={podnadpisma}>Njemóžu wotmołwy čitać, su pře małke.</h2>
       <p style={pismo}>Změn ZOOM faktor wobrazowki, na kompjuteru z STRG +</p>
       <br />
-      <h2 style={podnadpisma}>Maš dalše prašenja?<br /> Kontaktěruj nas přez mailku <a style={{color: 'green', textDecoration: 'none'}} href={`mailto:agata@lucija.de`}>agata@lucija.de</a></h2>
+      <h2 style={podnadpisma}>Maš dalše prašenja?<br /> Kontaktěruj nas přez mailku <a style={{color: 'cyan', textDecoration: 'none'}} href={`mailto:agata@lucija.de`}>agata@lucija.de</a></h2>
       <h2 style={{...podnadpisma, color: 'red', fontWeight: '1000'}}>Wutrobny dźak za pokiwy a konstruktiwnu kritiku.</h2>
     </div>
   )
